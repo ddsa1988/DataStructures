59c2b823af919f2bcc53618c526db7feb3a8abd7
Diego Alexandre <ddsa.1988@outlook.com>
2023-12-19 07:51:37 -0300

    commit

---
+++ CSharp/DataStructures/DataStructures/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataStructures.Stack;
+
+public class Program {
+    public static void Main(string[] args) {
+        UserStack<int> myStack = new UserStack<int>(10);
+        Console.WriteLine(myStack);
+
+        myStack.Push(20);
+        myStack.Push(30);
+        myStack.Push(40);
+
+        Console.WriteLine(myStack);
+        Console.WriteLine(myStack.Size());
+
+        myStack.Pop();
+        Console.WriteLine(myStack);
+        Console.WriteLine(myStack.Size());
+    }
+}
---
+++ CSharp/DataStructures/DataStructures/Stack/UserStack.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace DataStructures.Stack;
+
+public class UserStack<T> {
+
+    private readonly T[] stack;
+    private int counter = 0;
+
+    public UserStack(int size) {
+        stack = new T[size];
+    }
+
+    public void Push(T item) {
+        if (IsFull()) {
+            throw new Exception("Stack is full");
+        }
+
+        stack[counter++] = item;
+    }
+
+    public T Pop() {
+        if (IsEmpty()) {
+            throw new Exception("Stack is empty");
+        }
+
+        T item = stack[counter - 1];
+        stack[--counter] = default;
+
+        return item;
+    }
+
+    public int Size() {
+        return counter;
+    }
+
+    public bool IsEmpty() {
+        return Size() == 0;
+    }
+
+    public bool IsFull() {
+        return Size() == stack.Length;
+    }
+
+    public void Clear() {
+        Array.Clear(stack);
+        counter = 0;
+    }
+
+    public override string ToString() {
+        if (IsEmpty()) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder("[ ");
+
+        for (int i = 0; i < Size(); i++) {
+            sb.Append(stack[i] + " ");
+        }
+
+        sb.Append(']');
+
+        return sb.ToString();
+    }
+}
