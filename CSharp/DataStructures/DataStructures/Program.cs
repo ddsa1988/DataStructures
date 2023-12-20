02c754e2814bcc08be96fc4a53b9c606fadec91d
Diego Alexandre <ddsa.1988@outlook.com>
2023-12-20 20:52:47 -0300

    commit

--- CSharp/DataStructures/DataStructures/Program.cs
+++ CSharp/DataStructures/DataStructures/Program.cs
@@ -3,25 +3,5 @@
 namespace DataStructures;
 
 public class Program {
-    public static void Main(string[] args) {
-        UserDeque<float> deque = new UserDeque<float>();
-
-        deque.AddFront(10.5F);
-        deque.AddBack(11.5F);
-        deque.AddBack(22.5F);
-        Console.WriteLine(deque);
-        Console.WriteLine(deque.Size());
-        deque.AddFront(200F);
-        Console.WriteLine(deque);
-        Console.WriteLine(deque.Size());
-        Console.WriteLine(deque.RemoveFront());
-        Console.WriteLine(deque);
-        Console.WriteLine(deque.Size());
-        deque.AddFront(300F);
-        Console.WriteLine(deque);
-        Console.WriteLine(deque.Size());
-        Console.WriteLine(deque.RemoveBack());
-        Console.WriteLine(deque);
-        Console.WriteLine(deque.Size());
-    }
+    public static void Main(string[] args) { }
 }
